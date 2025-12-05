d35ff193a7431c538efae54d1d59857c72fb3ddf
pakhmutov <vladimir.pakhmutoff@gmail.com>
2025-12-06 01:07:31 +0300

    feat: init project

---
+++ README.md
@@ -0,0 +1,16 @@
+# @w8-ui/prettier-config
+
+## How to install ??
+
+```bash
+# or pnpm or yarn
+npm install -D @w8-ui/prettier-config prettier
+```
+
+## Usage ??
+
+Add `.prettierrc.js|ts` file in the project root with the following content:
+
+```js
+import { baseConfig } from "@w8-ui/prettier-config";
+```
---
+++ index.ts
@@ -0,0 +1,21 @@
+export const baseConfig = {
+    bracketSpacing: false,
+    tabWidth: 4,
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: "all",
+    overrides: [
+        {
+            files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+            options: {
+                parser: "typescript",
+            },
+        },
+        {
+            files: ["*.md", "*.json", "*.yaml", "*.yml"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
